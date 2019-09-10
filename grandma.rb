9efc20a8bc327a9422df2e5043e7af6ef58af174
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-10 17:48:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == "PHRASE"
-  puts "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
   else
-  puts "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
   end
 end
